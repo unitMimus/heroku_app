2a57370a02e60f39a5f6051426ad9a9103dcd6c0
unitMimus <stfyodorov@gmail.com>
2018-12-21 02:10:51 +0300

    add some wellcome text

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Get out of here. You'r not welcome."
       redirect_to @user
     else
       render 'new'
