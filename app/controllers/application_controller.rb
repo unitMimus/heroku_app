e79885608659c95d45d92d55362a4f5851178f03
unitMimus <stfyodorov@gmail.com>
2018-12-22 20:28:48 +0300

    add actions at login succes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "and here we now...again"
-  end
+  include SessionsHelper
 end
