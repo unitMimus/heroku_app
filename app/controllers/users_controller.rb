aa125d42beaa32843c3ef217b1627cd02b77d5a3
unitMimus <stfyodorov@gmail.com>
2018-12-22 21:48:02 +0300

    some changes in tests for correct logic of test_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Get out of here. You'r not wellcome."
       redirect_to @user
     else
