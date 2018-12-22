b806d4b601d2a81c3265595132910b64a7ece798
unitMimus <stfyodorov@gmail.com>
2018-12-22 22:02:29 +0300

    add logout code and tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,6 +16,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
+=begin
+  #not needed after adding login->logout test
   test "login with valid information" do
     get login_path
     post login_path, params: { session: { email: @user.email, password: 'password' } }
@@ -25,5 +27,26 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+  end
+=end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
