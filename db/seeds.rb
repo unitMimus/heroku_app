f7ac9f31127bea7606720904f8d2a96b1f09dedf
unitMimus <stfyodorov@gmail.com>
2018-12-24 02:12:10 +0300

    add administrative role

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "qweqwe",
-             password_confirmation: "qweqwe")
+             password_confirmation: "qweqwe",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
