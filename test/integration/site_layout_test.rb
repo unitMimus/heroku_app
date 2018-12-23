64b689d8e27e17da79377bb9fac99ad9df78c678
unitMimus <stfyodorov@gmail.com>
2018-12-24 01:33:58 +0300

    user list with test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,5 +13,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path, count: 0
+  end
+
+  test "user_path link for logged" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
   end
 end
