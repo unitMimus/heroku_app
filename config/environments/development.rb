edb81df57f23a8a8a4bb87f65177893326ff1ab5
unitMimus <stfyodorov@gmail.com>
2018-12-24 13:19:25 +0300

    activation mails preview

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,10 +26,17 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  # Local server
+  host = '0.0.0.0:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  # here wos for Cloud IDE, but i added heroku
+  #host = 'still-depths-46704.herokuapp.com'
+  #config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
-  config.action_mailer.perform_caching = false
+  #don't know if next string still needed or not, commented just in case
+  #config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
