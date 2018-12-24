68c2ae317582235ae8b555af8950075908f9d147
unitMimus <stfyodorov@gmail.com>
2018-12-24 19:28:10 +0300

    add last third of new pass validation test

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -56,7 +56,26 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
                     user: { password:              "foobaz",
                             password_confirmation: "foobaz" } }
     assert is_logged_in?
+    @user.update_attribute(:reset_digest, nil)
+    @user.reload
+    assert_nil @user.reset_digest
     assert_not flash.empty?
     assert_redirected_to user
+  end
+
+  test "expired token" do
+    get new_password_reset_path
+    post password_resets_path,
+         params: { password_reset: { email: @user.email } }
+
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+    patch password_reset_path(@user.reset_token),
+          params: { email: @user.email,
+                    user: { password:              "foobar",
+                            password_confirmation: "foobar" } }
+    assert_response :redirect
+    follow_redirect!
+    assert_match "expired", response.body
   end
 end
