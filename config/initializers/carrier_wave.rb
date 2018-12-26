3cbdeee1b6baebbb445d8c5f21f57c1916dcbbdf
unitMimus <stfyodorov@gmail.com>
2018-12-26 14:28:22 +0300

    second try to push carrier changes to heroku

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,10 +6,6 @@ if Rails.env.production?
       :aws_access_key_id     => 'AKIAJUUAETCYVMH2532A',
       :aws_secret_access_key => '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'
     }
-<<<<<<< HEAD
     config.fog_directory     =  'aws:s3:::sampapp-bucket'
-=======
-    config.fog_directory     =  'arn:aws:s3:::sampapp-bucket'
->>>>>>> bb2a3434ab6625d155d0e0f733867466ab516a0d
   end
 end
